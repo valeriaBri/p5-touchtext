cb1916c43671da6fe558dcf6dbeb2d2fe117aada
valeriaBri <valeria.brienza@gmail.com>
2016-12-06 15:37:52 +0100

    added code

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(200,50);
+  
+  var colorList = ['#e86584',
+                 '#3c5979',
+                 '#018cb7',
+                 '#fae093',
+                 '#ffe2e6',
+                 '#6eb66d',
+                 '#ffa960',
+                 '#2b61ce',
+                 '#c496d7',
+                 '#6faaec',
+                 '#fea24d'
+                 ];
+
+
+    //The list of touch points: touches
+    
+    for (var i = 0; i < touches.length; i++) {
+        var touch = touches [i]; 
+        
+        var touchColor = colorList[i];
+        fill(touchColor);
+        ellipse(touch.x, touch.y, 150)
+    }
+}
